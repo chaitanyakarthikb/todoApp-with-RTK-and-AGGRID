427a8cec304cbaff0d7ebcdce023b04093a3d70a
Basva Chaitanya Karthik <chaitanyakarthikbasva@gmail.com>
2026-01-30 15:23:15 +0530

    adding react strict mode

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
     <App />
     </Provider>
